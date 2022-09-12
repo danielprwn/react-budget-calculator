44bafbb2d6bb518edc302b12f0c39824199aeaeb
danielprwn <danielprwn@gmail.com>
2022-09-12 13:35:58 +0200

    add files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
+import { IncExp } from "./components/IncExp";
+import TransactionList from "./components/TransactionList";
+import { TransactionsAdd } from "./components/TransactionsAdd";
+
+import { GlobalProvider } from "./operations/GlobalState";
+
+import "./App.css";
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncExp />
+        <TransactionList />
+        <TransactionsAdd />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
